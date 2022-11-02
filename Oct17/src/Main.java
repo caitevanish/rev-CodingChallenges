eb9b8e5ab3d695d73dd70b6b2140c25ede747518
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-11-02 10:07:46 -0700

    Fixed project structure

--- Oct17/src/Main.java
+++ Oct17/src/Main.java
@@ -20,12 +20,21 @@ public class Main {
 
         list2.addFirst(99);
         list2.addLast(1);
-        list2.addLast(8);
+        list2.addLast(7);
         list2.addLast(11);
 
         System.out.println(LinkedList.getIntersection(list1,list2));
 
-//------Prompt 2: Stacks
+        list2.addLast(12);
+
+        System.out.println(LinkedList.getIntersection(list1,list2));
+
+        list1.addFirst(1);
+        list1.addLast(20);
+
+        System.out.println(LinkedList.getIntersection(list1,list2));
+
+        //------Prompt 2: Stacks
 
         //incomplete
     }
